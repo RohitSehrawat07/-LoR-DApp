97c8f471f90194567e245e07a0b8abb175da5f04
RohitSehrawat07 <rohitsehrawat074@gmail.com>
2026-06-29 19:13:32 +0530

    Add contract address and ABI definitions

---
+++ contract.js
@@ -0,0 +1,9 @@
+export const contractAddress =
+"YOUR_DEPLOYED_CONTRACT_ADDRESS";
+
+export const abi = [
+  "function addStudent(string,string,string)",
+  "function requestRecommendation(uint256)",
+  "function approveRecommendation(uint256)",
+  "function getStudent(uint256) view returns(uint256,string,string,string,bool,bool)"
+];
